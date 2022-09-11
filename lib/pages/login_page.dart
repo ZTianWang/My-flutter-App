4e652814906e44e60c38be11fd58d5785e8b7f0a
rayaway2020 <69098495+rayaway2020@users.noreply.github.com>
2022-09-12 01:24:00 +1200

    Routing error fixed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -109,7 +109,7 @@ class _LoginPageState extends State<LoginPage> {
                             context,
                             MaterialPageRoute(
                                 builder: (context) =>
-                                    const SelectInterestPage()))
+                                    const HomeScreen()))
                       },
                       child: Text(
                         "Continue",
