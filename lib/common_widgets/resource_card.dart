c475b8cbae4160cfd4951084df8dd3073a0b2556
rayaway2020 <69098495+rayaway2020@users.noreply.github.com>
2022-09-12 01:52:21 +1200

    Route to Category detail page from category card

--- lib/common_widgets/resource_card.dart
+++ lib/common_widgets/resource_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getskills_flutter/utils/global.dart';
+import 'package:getskills_flutter/pages/category_detail_page.dart';
 
 class ResourceCard extends StatelessWidget {
   final String title;
@@ -15,26 +16,34 @@ class ResourceCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(right: 20),
-      width: 230,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 5),
-          Text(
-            subtitle,
-            style: const TextStyle(color: titleColor),
-          ),
-          Image(
-            image: AssetImage(imageUrl),
-          )
-        ],
-      ),
+      child: GestureDetector(
+                onTap: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CategoryDetailPage()))
+                },
+                child: Container(
+                  padding: const EdgeInsets.only(right: 20),
+                  width: 230,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        subtitle,
+                        style: const TextStyle(color: titleColor),
+                      ),
+                      Image(
+                        image: AssetImage(imageUrl),
+                      )
+                    ],
+                  ),
+                ),
+              ),
     );
   }
 }
